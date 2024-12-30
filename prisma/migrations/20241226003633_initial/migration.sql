d5d436a68d87384da7e26e6c5b1c1ca709f19ce1
brayanpesantes <brayalexis.30@gmail.com>
2024-12-30 01:38:02 -0500

    Add Prisma setup and initial database schema; update dependencies and SVG assets

---
+++ prisma/migrations/20241226003633_initial/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "Board" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "icon" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "boardId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Task_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
