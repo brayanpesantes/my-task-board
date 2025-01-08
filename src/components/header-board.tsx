eb2e7d3dc4c93d7b187f4903161fca5de8b851d8
brayanpesantes <brayalexis.30@gmail.com>
2025-01-07 23:38:11 -0500

    Refactor HeaderBoard component layout for improved styling; reduce gap between elements for a cleaner appearance.

--- src/components/header-board.tsx
+++ src/components/header-board.tsx
@@ -9,16 +9,14 @@ interface Props {
 
 export const HeaderBoard = ({ board }: Props) => {
   return (
-    <div className="flex flex-col gap-4 items-start pb-10">
-      <div className="flex-col items-center gap-3">
-        <div className="inline-flex gap-2 items-center">
-          <Image src="/logo.svg" width={50} height={50} alt="logo task" />
-          <h1 className="text-4xl font-bold">{board.name}</h1>
-          <BoardDialog board={board} />
-        </div>
-        <div className="ml-16">
-          <p className="text-gray-600">{board.description}</p>
-        </div>
+    <div className="flex flex-col gap-3 items-start">
+      <div className="inline-flex gap-2 items-center">
+        <Image src="/logo.svg" width={50} height={50} alt="logo task" />
+        <h1 className="text-4xl font-bold">{board.name}</h1>
+        <BoardDialog board={board} />
+      </div>
+      <div className="ml-16">
+        <p className="text-gray-600">{board.description}</p>
       </div>
     </div>
   );
